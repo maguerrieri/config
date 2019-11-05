8dc5e5d2169093eeeaa5dba7900e33504c17dc5f
Mario Guerrieri <mario@guerrieri.codes>
2019-11-05 13:40:03 -0800

    Add hiding stuff to `setup.sh`

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 ./symlinks.py
 
-chflags +hidden $HOME/Dropbox*
+chflags hidden $HOME/Dropbox*
+chflags -h hidden $HOME/icloud
