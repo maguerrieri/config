6691726881be71dab5a7ce4d9b511f77328b63de
Mario Guerrieri <mario@guerrieri.codes>
2021-10-19 21:17:29 -0700

    Add `winget` completion

--- profile/powershell.ps1
+++ profile/powershell.ps1
@@ -21,6 +21,17 @@ function web ($arg) {
 	explorer $arg
 }
 
+# winget completion
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+
 # set up SSH agent
 # run 
 # 	> Unblock-File ssh-agent-utils.ps1
